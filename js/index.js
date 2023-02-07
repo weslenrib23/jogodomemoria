9fe034b1884bbb26e7bd9857dabdc5bb00be25f4
Wesley <weslenribeiro@hotmail.com>
2023-02-06 22:35:45 -0300

    Alterando de codigo, incluindo o JS e Pontuação

--- js/index.js
+++ js/index.js
@@ -3,6 +3,59 @@ var cartoes = document.querySelectorAll(".cartao");
 var existeCartaoVirado = false;
 var primeiroCartao, segundoCartao;
 
+var resultadosJogadores = [];
+
+var itensStorage = localStorage.getItem("resultado");
+
+if (itensStorage) {
+  var resultadosExistentes = JSON.parse(itensStorage);
+} else {
+  var resultadosExistentes = [];
+}
+
+console.log(resultadosJogadores);
+
+var placar = 0;
+var tempo = 0;
+
+var intervalo = setInterval(function () {
+  if (tempo == 15) {
+    clearInterval(intervalo);
+    alert("Tempo esgotado!");
+    var nome = prompt("Digite o seu nome:");
+    //Criar um objeto
+    var resultado = {
+      nomeJogador: nome,
+      pontuacao: placar,
+    };
+
+    resultadosExistentes.push(resultado);
+    localStorage.setItem("resultado", JSON.stringify(resultadosExistentes));
+
+    document.getElementById("recordes").style.display = "block";
+
+    var tabela = document.getElementById("resultados");
+
+    resultadosExistentes.sort().forEach(function (resultado) {
+      var tr = document.createElement("tr");
+      var tdNome = document.createElement("td");
+      tdNome.innerHTML = resultado.nomeJogador;
+      var tdPontuacao = document.createElement("td");
+      tdPontuacao.innerHTML = resultado.pontuacao;
+      tr.appendChild(tdNome);
+      tr.appendChild(tdPontuacao);
+      tabela.appendChild(tr);
+    });
+
+    cartoes.forEach(function (cartao) {
+      cartao.removeEventListener("click", virarCartao);
+    });
+    return;
+  }
+  tempo = tempo + 1;
+  document.getElementById("tempo").innerHTML = tempo;
+}, 1000);
+
 function virarCartao() {
   this.classList.add("virado");
 
@@ -16,8 +69,16 @@ function virarCartao() {
     if (primeiroCartao.dataset.info === segundoCartao.dataset.info) {
       primeiroCartao.removeEventListener("click", virarCartao);
       segundoCartao.removeEventListener("click", virarCartao);
+      placar = placar + 1;
+      document.getElementById("resultado").innerHTML = placar;
     } else {
+      cartoes.forEach(function (cartao) {
+        cartao.removeEventListener("click", virarCartao);
+      });
       setTimeout(function () {
+        cartoes.forEach(function (cartao) {
+          cartao.addEventListener("click", virarCartao);
+        });
         primeiroCartao.classList.remove("virado");
         segundoCartao.classList.remove("virado");
       }, 2000);
